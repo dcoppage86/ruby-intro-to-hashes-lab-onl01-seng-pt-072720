7ab773f46b6e2cd1fd56feca907c42e060c52591
Dakota Coppage <dakotakcoppage@me.com>
2020-08-09 23:43:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,13 +27,13 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly = {
-    railroads: {
-      :pieces => 4, 
-      :rent_in_dollars => {}, 
-      :names => {}
-    }
-  }
+	monopoly = {
+	  :railroads =>
+	  {:pieces => 4, 
+	  :names => {}, 
+	  :rent_in_dollars => {}
+		}
+	}
 
 end
 
